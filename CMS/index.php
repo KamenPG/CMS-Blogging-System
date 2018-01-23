0e5aa73f1ae0c258c0f0a2ed9348429e103e55a9
KamenPG <kamen971@gmail.com>
2018-01-23 14:05:56 +0200

    CMS version 1.0

---
+++ CMS/index.php
@@ -0,0 +1,30 @@
+<?php include "includes/db.php"; global $connect; ?>
+
+<?php include "includes/header.php"; ?>
+
+    <!-- Navigation -->
+
+    <?php include "includes/navigation.php"; ?>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+
+            <!-- Blog Entries Column -->
+            <div class="col-md-8">
+
+              <?php include "includes/search.php" ?>
+
+              </div>
+
+            <!-- Blog Sidebar Widgets Column -->
+
+            <?php include "includes/sidebar.php" ?>
+
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+        <?php include "includes/footer.php"; ?>
