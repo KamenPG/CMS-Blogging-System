3e050412a22257923128a9f03801b2f744daff7b
KamenPG <kamen971@gmail.com>
2018-03-13 20:27:09 +0200

    Adding some screenshots of the project

--- CMS/includes/footer.php
+++ CMS/includes/footer.php
@@ -2,7 +2,7 @@
 <footer>
     <div class="row">
         <div class="col-lg-12">
-            <p>Copyright &copy; Your Website 2018</p>
+            <p>Copyright &copy; KamenPG Website</p>
         </div>
         <!-- /.col-lg-12 -->
     </div>
